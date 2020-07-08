b4c2716c5673628384bc590f455c56d3e1fe8e2e
Chester Chee <chester.chee@gmail.com>
2020-07-08 17:10:23 -0400

    Update

--- codec/__init__.py
+++ codec/__init__.py
@@ -1,27 +1,19 @@
-# moldudp_codec
-from moldudp_codec.const import END_OF_SESSION
-from moldudp_codec.const import HEART_BEAT
-from moldudp_codec.const import HEADER_SIZE
-from moldudp_codec.const import MESSAGE_SIZE_FIELD_LEN
-from moldudp_codec.const import MOLDPKT_SIZE
-from moldudp_codec.const import PAYLOAD_OFFSET
-from moldudp_codec.const import PAYLOAD_SIZE
-from moldudp_codec.const import SESSION_OFFSET
-from moldudp_codec.decoder import MoldUDPDecoder
-from moldudp_codec.encoder import MoldUDPEncoder
-from moldudp_codec.msgpub import MsgPublisher
-from moldudp_codec.msgsub import MsgSubscriber
+# codec
+import const
+import decoder
+import encoder
+import msgpub
+import msgsub
 
-__all__ = [
-    'END_OF_SESSION',
-    'HEART_BEAT',
-    'HEADER_SIZE',
-    'MESSAGE_SIZE_FIELD_LEN',
-    'MOLDPKT_SIZE',
-    'PAYLOAD_OFFSET',
-    'PAYLOAD_SIZE',
-    'MoldUDPDecoder',
-    'MoldUDPEncoder',
-    'MsgPublisher',
-    'MsgSubscriber'
-]
+END_OF_SESSION = const.END_OF_SESSION
+HEART_BEAT = const.HEART_BEAT
+HEADER_SIZE = const.HEADER_SIZE
+MESSAGE_SIZE_FIELD_LEN = const.MESSAGE_SIZE_FIELD_LEN
+MOLDPKT_SIZE = const.MOLDPKT_SIZE
+PAYLOAD_OFFSET = const.PAYLOAD_OFFSET
+PAYLOAD_SIZE = const.PAYLOAD_SIZE
+SESSION_OFFSET = const.SESSION_OFFSET
+MoldUDPDecoder = decoder.MoldUDPDecoder
+MoldUDPEncoder = encoder.MoldUDPEncoder
+MsgPublisher = msgpub.MsgPublisher
+MsgSubscriber = msgsub.MsgSubscriber
--- install/upgrade.sh
+++ install/upgrade.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 version=${1}
 
-python3 -m pip uninstall moldudp-codec
+pip3 uninstall -y moldudp-codec
 python3 -m pip install -i https://test.pypi.org/simple/ moldudp-codec==$version
