f5fea489da4c0e3eee7f2d408b327a56021a3b12
Chester Chee <chester.chee@gmail.com>
2020-07-08 18:23:37 -0400

    Update so we can also upload to official package repo

--- install/deploy.sh
+++ install/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+repo=${1:-testpypi}
 
 rm -rf build dist moldudp_codec.egg-info
 python3 setup.py sdist bdist_wheel
-python3 -m twine upload --repository testpypi dist/*
+python3 -m twine upload --repository $repo dist/*
--- install/upgrade.sh
+++ install/upgrade.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 version=${1}
+repo_url=${2:-https://test.pypi.org/simple}
 
 pip3 uninstall -y moldudp
-python3 -m pip install -i https://test.pypi.org/simple/ moldudp==$version
+python3 -m pip install -i $repo_url/ moldudp==$version
