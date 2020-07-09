ee99caba32ea0a304737bcaa66392c8de137af33
Chester Chee <chester.chee@gmail.com>
2020-07-09 08:35:57 -0400

    Reorg

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, "README.md"), "r") as fh:
 
 setup(
     name='moldudp',
-    version='0.0.20',
+    version='0.0.21',
     description='A simple library to decode/encode MoldUDP64 bytearray/packet',
     long_description=long_description,
     long_description_content_type="text/markdown",
