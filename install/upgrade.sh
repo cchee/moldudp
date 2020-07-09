d0caecd75b619f831bf5053b960ea9d6d8e6984b
Chester Chee <chester.chee@gmail.com>
2020-07-09 08:07:20 -0400

    Updated upgrade.sh

--- install/upgrade.sh
+++ install/upgrade.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 version=${1}
-repo_url=${2:-https://test.pypi.org/simple}
+repo=${2:-testpypi}
+test_repo_url=https://test.pypi.org/simple
 
 pip3 uninstall -y moldudp
-python3 -m pip install -i $repo_url/ moldudp==$version
+if [ "${repo}" == "pypi" ]; then
+    pip3 install moldudp==$version
+else
+    pip3 install -i $test_repo_url/ moldudp==$version
+fi
